460cb17e43ac785f08218f674bb9ab14eae42656
Zé Mário <59663635+josemariofr@users.noreply.github.com>
2023-10-10 17:36:07 -0300

    ex004

---
+++ ex004.py
@@ -0,0 +1,8 @@
+a = input('Digite alguma coisa: ')  
+print('O tipo primitivo dese valor é', type(a))
+print('Só tem espaços? ', a.isspace()) 
+print('Só tem números? ', a.isnumeric())
+print('É alfabético? ', a.isalpha()) 
+print('É alfanumérico? ', a.isalnum())
+print('É maiúcula? ', a.isupper())                 
+print('É minúscula? ', a.islower())
