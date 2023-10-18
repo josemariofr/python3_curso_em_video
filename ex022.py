91847151b30ee494c0ab75dcc1e7ff3751066287
Zé Mário <josemariofr@live.com>
2023-10-18 20:51:30 -0300

    ex022

---
+++ ex022.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite seu nome: ')).strip()
+print('Analisando seu nome...')
+print('Seu nome em letras maiúsculas é {}'.format(nome.upper()))
+print('Seu nome em letras minúsculas é {}'.format(nome.lower()))
+print('Seu nome completo contém {} letras'.format(len(nome) - nome.count(' ')))
+print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
