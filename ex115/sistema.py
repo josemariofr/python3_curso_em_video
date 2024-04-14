423ed324bbfc878c7bb35b2e3644a344916be854
Zé Mário <josemariofr@live.com>
2024-04-14 16:57:07 -0300

    ex115b

--- ex115/sistema.py
+++ ex115/sistema.py
@@ -1,14 +1,21 @@
 from biblioteca.interf import *
+from biblioteca.arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+    
 
 while True:
     resp = menu(['Novo Arquivo', 'Cadastro', 'Listar Usuários', 'Sair'])
     if resp == 1:
-        cabeçalho ('Opção 1')
+        cabeçalho('Opção 1')
     elif resp == 2:
         cabeçalho('Opção 2')
     elif resp == 3:
-        cabeçalho('Opção 3')
+        lerArquivo(arq)
     elif resp == 4:
         cabeçalho('Saindo...')
         sleep(2)
