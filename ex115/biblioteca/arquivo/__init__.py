bf70fb74ad7969cc331cecd54bf07a014ce32e78
Zé Mário <josemariofr@live.com>
2024-04-15 20:35:04 -0300

    ex115a,b,c

--- ex115/biblioteca/arquivo/__init__.py
+++ ex115/biblioteca/arquivo/__init__.py
@@ -25,7 +25,26 @@ def lerArquivo(nome):
     try:
         a = open(nome, 'rt')
     except:
-        print(('Erro ao ler arquivo'))
+        print('Erro ao ler arquivo')
     else:
         cabeçalho('PESSOAS ENCONTRADAS')
-        print(a.read)
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<30}{dado[1]:>3} anos')
+    finally:
+        a.close()
+
+def cadastrar(arq, nome='desconhecido', idade=0):
+    try:
+        a = open(arq, 'at')
+    except:
+        print('Houve um ERRO na abertura do arquivo!')
+    else:
+        try:
+            a.write(f'{nome};{idade}\n')
+        except:
+            print('Houve um ERRO na hora de escrever os dados.')
+        else:
+            print(f'Novo registro de {nome} adicionado.')
+            a.close
--- ex115/sistema.py
+++ ex115/sistema.py
@@ -5,15 +5,17 @@ from time import sleep
 arq = 'cursoemvideo.txt'
 
 if not arquivoExiste(arq):
-    criarArquivo(arq)
+    criarArquivo(arq)   
     
-
 while True:
     resp = menu(['Novo Arquivo', 'Cadastro', 'Listar Usuários', 'Sair'])
     if resp == 1:
         cabeçalho('Opção 1')
     elif resp == 2:
-        cabeçalho('Opção 2')
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resp == 3:
         lerArquivo(arq)
     elif resp == 4:
